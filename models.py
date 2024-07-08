189ad25e18f5543b84fa917f9dcd8bcd83b9b927
MarcosAyrton <m.geller@itecriocuarto.org.ar>
2024-07-08 19:01:49 -0300

    carga de models

--- models.py
+++ models.py
@@ -1,104 +1,104 @@
+# app/models.py
+
 from app import db
-from sqlalchemy import Numeric, ForeignKey, Boolean
+from sqlalchemy import ForeignKey, Boolean, String, Float, Integer
 from sqlalchemy.orm import relationship
 
 class Equipo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    modelo_id = db.Column(db.Integer, db.ForeignKey("modelo.id"), nullable=False)
-    categoria_id = db.Column(db.Integer, db.ForeignKey("categoria.id"), nullable=False)
-    fabricante_id = db.Column(db.Integer, db.ForeignKey("fabricante.id"), nullable=False)
-    caracteristicas_id = db.Column(db.Integer, db.ForeignKey("caracteristicas.id"), nullable=False)
-    marca_id = db.Column(db.Integer, db.ForeignKey("marca.id"), nullable=False)
-    stock_id = db.Column(db.Integer, db.ForeignKey("stock.id"), nullable=False)
-    costo = db.Column(db.Float)
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    modelo_id = db.Column(Integer, db.ForeignKey("modelo.id"), nullable=False)
+    fabricante_id = db.Column(Integer, db.ForeignKey("fabricante.id"), nullable=False)
+    caracteristicas_id = db.Column(Integer, db.ForeignKey("caracteristicas.id"), nullable=False)
+    marca_id = db.Column(Integer, db.ForeignKey("marca.id"), nullable=False)
+    stock_id = db.Column(Integer, db.ForeignKey("stock.id"), nullable=False)
+    costo = db.Column(Float)
 
-    modelo = relationship('Modelo', back_populates='equipo')
-    categoria = relationship('Categoria', back_populates='equipo')
-    fabricante = relationship('Fabricante', back_populates='equipo')
-    caracteristicas = relationship('Caracteristicas', back_populates='equipo')
-    stock = relationship('Stock', back_populates='equipo')
-    marca = relationship('Marca', back_populates='equipo')
+    modelo = relationship('Modelo', back_populates='equipos')
+    fabricante = relationship('Fabricante', back_populates='equipos')
+    caracteristicas = relationship('Caracteristicas', back_populates='equipos')
+    stock = relationship('Stock', back_populates='equipos')
+    marca = relationship('Marca', back_populates='equipos')
 
-    def _str_(self):
+    def __str__(self):
         return self.nombre
-        
 
 class Modelo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    fabricante = db.Column(db.String(50), nullable=False)
-    marca_id = db.Column(db.Integer, db.ForeignKey("marca.id"), nullable=False)
-    equipo_id = db.Column(db.Integer, db.ForeignKey("equipo.id"), nullable=False)
-    descripcion = db.Column(db.String(100), nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    fabricante = db.Column(String(50), nullable=False)
+    marca_id = db.Column(Integer, db.ForeignKey("marca.id"), nullable=False)
+    descripcion = db.Column(String(100), nullable=False)
 
-    marca = relationship('Marca', back_populates='modelo')
-    equipo = relationship('Equipo', back_populates='modelo')
-    
-    def _str_(self):
+    marca = relationship('Marca', back_populates='modelos')
+    equipos = relationship('Equipo', back_populates='modelo')
+
+    def __str__(self):
         return self.nombre
 
-
 class Marca(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    fabricante_id = db.Column(db.Integer, db.ForeignKey("fabricante.id"), nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    fabricante_id = db.Column(Integer, db.ForeignKey("fabricante.id"), nullable=False)
     
-    
-    fabricante = relationship('Fabricante', back_populates='marca')
-    equipo = relationship('Equipo', back_populates='marca')
+    fabricante = relationship('Fabricante', back_populates='marcas')
+    equipos = relationship('Equipo', back_populates='marca')
+    modelos = relationship('Modelo', back_populates='marca')
 
-    def _str_(self):
+    def __str__(self):
         return self.nombre 
 
 class Fabricante(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    pais_origen = db.Column(db.String(70),nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    pais_origen = db.Column(String(70), nullable=False)
 
-    marca = relationship('Marca', back_populates='fabricante')
-    equipo = relationship('Equipo', back_populates='fabricante')
+    marcas = relationship('Marca', back_populates='fabricante')
+    equipos = relationship('Equipo', back_populates='fabricante')
 
-    def _str_(self):
+    def __str__(self):
         return self.nombre 
 
 class Caracteristicas(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    color = db.Column(db.String(50), nullable=False)
-    pantalla = db.Column(db.String(50), nullable=False)
-    bateria = db.Column(db.String(50), nullable=False)
-    camara = db.Column(db.String(50), nullable=False)
-    cpu  = db.Column(db.String(50), nullable=False)
-    software = db.Column(db.String(50), nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    color = db.Column(String(50), nullable=False)
+    pantalla = db.Column(String(50), nullable=False)
+    bateria = db.Column(String(50), nullable=False)
+    camara = db.Column(String(50), nullable=False)
+    cpu = db.Column(String(50), nullable=False)
+    software = db.Column(String(50), nullable=False)
 
-    equipo = relationship('Equipo', back_populates='caracteristicas')
+    equipos = relationship('Equipo', back_populates='caracteristicas')
 
-    def _str_(self):
+    def __str__(self):
         return self.id 
-    
-class Stock(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cantidad = db.Column(db.Float)
-    disponibilidad = db.Column(db.Boolean, default=True)
-    ubicacion = db.Column(db.String(50), nullable=False)
-    
-    equipo = relationship('Equipo', back_populates='stock')
 
-    def _str_(self):
-        return self.cantidad
+class Stock(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    cantidad = db.Column(Float)
+    disponibilidad = db.Column(Boolean, default=True)
+    ubicacion = db.Column(String(50), nullable=False)
+    
+    equipos = relationship('Equipo', back_populates='stock')
+
+    def __str__(self):
+        return str(self.cantidad)
 
 class Proveedor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    Num_telefon = db.Column(db.Float)
-    provincia = db.Column(db.String(50), nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    num_telefono = db.Column(Float)
+    provincia = db.Column(String(50), nullable=False)
     
-    def _str_(self):
+    def __str__(self):
         return self.nombre
 
 class Accesorio(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cargador = db.Column(db.Boolean, default=True)
-    auriculares = db.Column(db.Boolean, default=True)
-    funda = db.Column(db.Boolean, default=True)
-    protector_de_pantalla = db.Column(db.Boolean, default=True)
+    id = db.Column(Integer, primary_key=True)
+    cargador = db.Column(Boolean, default=True)
+    auriculares = db.Column(Boolean, default=True)
+    funda = db.Column(Boolean, default=True)
+    protector_de_pantalla = db.Column(Boolean, default=True)
+
+    def __str__(self):
+        return str(self.id)
