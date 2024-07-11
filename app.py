35c5830c12a8fdb1206f225e3d2976bdff7f45e4
tom1mvp <tomasbratik83@gmail.com>
2024-07-11 17:31:16 -0300

    subida de la seccion agregar_producto

--- app.py
+++ app.py
@@ -10,15 +10,131 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Agregar el from models import
+from models import (
+    Equipo,
+    Modelo,
+    Marca,
+    Fabricante,
+    Caracteristicas,
+    Stock,
+    Proveedor,
+    Accesorio
+)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/producto")
-def prendas():
+def producto():
     return render_template('producto.html')
 
-@app.route("/agregar_producto")
-def prendas():
+@app.route("/agregar_producto", methods=['POST', 'GET'])
+def agregarProductos():
+    if request.method == 'POST':
+        equipo = request.form['nombre_equipo']
+        modelo = request.form['nombre_modelo']
+        marca = request.form['nombre_marca']
+        fabricante = request.form['nombre_fabricante']
+        color = request.form['color']
+        pantalla = request.form['pantalla_tamaño']
+        bateria = request.form['bateria']
+        camara = request.form['camara']
+        cpu = request.form['cpu']
+        software = request.form['tipo_software']
+        stock = request.form['stock']
+        precio = request.form['precio']
+        cargador = request.form['cargador']
+        auriculares = request.form['auriculares']
+        funda = request.form['funda']
+        ubicacion = request.form['ubicacion_del_equipo']
+        protector = request.form['protector']
+        pais = request.form['pais_del_fabricante']
+        descripcion_equipo = request.form['descripcion_del_equipo']
+        cantidad = request.form['cantidad_equipo']
+        provincia = request.form['provincia_prov']
+        num_prov = request.form['numero_de_prov']
+        proveedor = request.form['nombre_proveedor']
+
+        fabricante_db = Fabricante(
+            nombre=fabricante,
+            pais_origen=pais
+        )
+        db.session.add(fabricante_db)
+        db.session.commit()
+
+        marca_db = Marca(
+            nombre=marca,
+            fabricante=fabricante_db
+        )
+
+        db.session.add(marca_db)
+        db.session.commit()
+
+        modelo_db = Modelo(
+            nombre=modelo,
+            fabricante=fabricante_db.nombre,
+            marca=marca_db,
+            descripcion=descripcion_equipo
+        )
+        db.session.add(modelo_db)
+        db.session.commit()
+
+        caracteristicas_db = Caracteristicas(
+            color=color,
+            pantalla=pantalla,
+            bateria=bateria,
+            camara=camara,
+            cpu=cpu,
+            software=software
+        )
+        db.session.add(caracteristicas_db)
+        db.session.commit()
+
+        stock_db = Stock(
+            cantidad=1 if stock == 'si' else 0,
+            disponibilidad=True if stock == 'si' else False,
+            ubicacion=ubicacion
+        )
+
+        db.session.add(stock_db)
+        db.session.commit()
+
+        equipo_db = Equipo(
+            nombre=equipo,
+            modelo=modelo_db,
+            marca=marca_db,
+            fabricante=fabricante_db,
+            caracteristicas=caracteristicas_db,
+            stock=stock_db,
+            costo=float(precio)
+        )
+
+        db.session.add(equipo_db)
+        db.session.commit()
+
+        proveedor_db = Proveedor(
+            nombre=proveedor,
+            num_telefono=num_prov,
+            provincia=provincia
+        )
+        db.session.add(proveedor_db)
+        db.session.commit()
+
+        accesorios_db = Accesorio(
+            cargador=True if cargador == 'si' else False,
+            auriculares=True if auriculares == 'si' else False,
+            funda=True if funda == 'si' else False,
+            protector_de_pantalla=True if protector == 'si' else False
+        )
+
+        db.session.add(accesorios_db)
+        db.session.commit()
+
+        return redirect('/stock')  # Redirige a 'Stock' una vez guardado lo ingresado
     return render_template('agregar_producto.html')
+
+
+@app.route("/stock")
+def mostrarStock():
+    return render_template('stock.html')
--- models.py
+++ models.py
@@ -1,4 +1,105 @@
 from app import db
-from sqlalchemy import Numeric, ForeignKey
+from sqlalchemy import ForeignKey, Boolean, String, Float, Integer
 from sqlalchemy.orm import relationship
 
+class Equipo(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    modelo_id = db.Column(Integer, db.ForeignKey("modelo.id"), nullable=False)
+    fabricante_id = db.Column(Integer, db.ForeignKey("fabricante.id"), nullable=False)
+    caracteristicas_id = db.Column(Integer, db.ForeignKey("caracteristicas.id"), nullable=False)
+    marca_id = db.Column(Integer, db.ForeignKey("marca.id"), nullable=False)
+    stock_id = db.Column(Integer, db.ForeignKey("stock.id"), nullable=False)
+    costo = db.Column(Float)
+
+    modelo = relationship('Modelo', back_populates='equipos')
+    fabricante = relationship('Fabricante', back_populates='equipos')
+    caracteristicas = relationship('Caracteristicas', back_populates='equipos')
+    stock = relationship('Stock', back_populates='equipos')
+    marca = relationship('Marca', back_populates='equipos')
+
+    def _str_(self):
+        return self.nombre
+
+class Modelo(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(100), nullable=False)
+    fabricante = db.Column(String(100), nullable=False)
+    marca_id = db.Column(Integer, db.ForeignKey("marca.id"), nullable=False)
+    descripcion = db.Column(String(100), nullable=False)
+
+    marca = relationship('Marca', back_populates='modelos')
+    equipos = relationship('Equipo', back_populates='modelo')
+
+    def _str_(self):
+        return self.nombre
+
+class Marca(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(50), nullable=False)
+    fabricante_id = db.Column(Integer, db.ForeignKey("fabricante.id"), nullable=False)
+    
+    fabricante = relationship('Fabricante', back_populates='marcas')
+    equipos = relationship('Equipo', back_populates='marca')
+    modelos = relationship('Modelo', back_populates='marca')
+
+    def _str_(self):
+        return self.nombre 
+
+class Fabricante(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(100), nullable=False)
+    pais_origen = db.Column(String(100), nullable=False)
+
+    marcas = relationship('Marca', back_populates='fabricante')
+    equipos = relationship('Equipo', back_populates='fabricante')
+
+    def __str__(self):
+        return self.nombre 
+
+
+class Caracteristicas(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    color = db.Column(String(80), nullable=False)
+    pantalla = db.Column(String(100), nullable=False)
+    bateria = db.Column(String(50), nullable=False)
+    camara = db.Column(String(100), nullable=False)
+    cpu = db.Column(String(80), nullable=False)
+    software = db.Column(String(50), nullable=False)
+
+    equipos = relationship('Equipo', back_populates='caracteristicas')
+
+    def _str_(self):
+        return self.id 
+
+class Stock(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    cantidad = db.Column(Float)
+    disponibilidad = db.Column(Boolean, default=True)
+    ubicacion = db.Column(String(100), nullable=False)
+    
+    equipos = relationship('Equipo', back_populates='stock')
+
+    def _str_(self):
+        return str(self.cantidad)
+
+class Proveedor(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    nombre = db.Column(String(100), nullable=False)  # Asegúrate de que el campo se llame 'nombre' aquí
+    num_telefono = db.Column(Float)
+    provincia = db.Column(String(100), nullable=False)
+    
+    def __str__(self):
+        return self.nombre
+
+
+class Accesorio(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    cargador = db.Column(Boolean, default=True)
+    auriculares = db.Column(Boolean, default=True)
+    funda = db.Column(Boolean, default=True)
+    protector_de_pantalla = db.Column(Boolean, default=True)
+
+    def _str_(self):
+        return str(self.id)
+    
